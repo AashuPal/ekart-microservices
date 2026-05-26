4ad3feaf9ce9bb97e4e16517ed913327e1d98a5f
Aashu Pal <aashup838@gmail.com>
2026-05-27 00:13:12 +0530

    Crossorigin fix

--- auth-service/src/main/java/com/infy/ekart/config/CorsConfig.java
+++ auth-service/src/main/java/com/infy/ekart/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080","http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:8080","http://localhost:5173","https://ekart-frontend-g20a.onrender.com"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
