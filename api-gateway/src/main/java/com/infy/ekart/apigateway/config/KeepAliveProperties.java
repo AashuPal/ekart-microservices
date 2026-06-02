86ef64d1b849037e74b48a109d956074cbf78b26
AashuPal <92346432+AashuPal@users.noreply.github.com>
2026-06-02 11:31:10 +0530

    Update KeepAliveProperties.java

--- api-gateway/src/main/java/com/infy/ekart/apigateway/config/KeepAliveProperties.java
+++ api-gateway/src/main/java/com/infy/ekart/apigateway/config/KeepAliveProperties.java
@@ -2,13 +2,11 @@ package com.infy.ekart.apigateway.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@Component
 @ConfigurationProperties(prefix = "app.keep-alive")
 public class KeepAliveProperties {
     private boolean enabled = false;
