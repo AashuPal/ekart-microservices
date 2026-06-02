3240a9da11acee5137e269440f73938491675afc
AashuPal <92346432+AashuPal@users.noreply.github.com>
2026-06-02 11:22:33 +0530

    Update ApiGatewayApplication.java

--- api-gateway/src/main/java/com/infy/ekart/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/infy/ekart/apigateway/ApiGatewayApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import com.infy.ekart.apigateway.config.KeepAliveProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 @EnableDiscoveryClient
 @EnableScheduling
 @SpringBootApplication
