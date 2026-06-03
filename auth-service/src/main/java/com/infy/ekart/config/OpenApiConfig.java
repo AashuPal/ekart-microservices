9f9a11856624a7bdd30a0c1f629ed828309e103b
AashuPal <92346432+AashuPal@users.noreply.github.com>
2026-06-03 07:43:05 +0530

    Update OpenApiConfig.java

--- auth-service/src/main/java/com/infy/ekart/config/OpenApiConfig.java
+++ auth-service/src/main/java/com/infy/ekart/config/OpenApiConfig.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
