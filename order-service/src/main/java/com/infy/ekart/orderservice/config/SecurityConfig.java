130c5d28fea0513b55fd0683ef55b65fa33f45b1
AashuPal <92346432+AashuPal@users.noreply.github.com>
2026-06-06 16:20:37 +0530

    Update SecurityConfig.java

--- order-service/src/main/java/com/infy/ekart/orderservice/config/SecurityConfig.java
+++ order-service/src/main/java/com/infy/ekart/orderservice/config/SecurityConfig.java
@@ -5,11 +5,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
+    //@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
