d6387ced3fab77fdc9da32ce6b83a0b16c9edfbc
AashuPal <92346432+AashuPal@users.noreply.github.com>
2026-06-06 13:17:15 +0530

    Update CorsConfig.java

--- auth-service/src/main/java/com/infy/ekart/config/CorsConfig.java
+++ auth-service/src/main/java/com/infy/ekart/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080","https://api-gateway-p817.onrender.com","https://ekartms.netlify.app/"));
+        config.setAllowedOrigins(List.of("http://localhost:8080","http://localhost:5173/","https://api-gateway-p817.onrender.com","https://ekartms.netlify.app/"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
